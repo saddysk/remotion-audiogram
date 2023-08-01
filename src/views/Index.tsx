10d6342c176536584acf2b70714bc2a7216f4fe0
saddysk <sksaddamh08@gmail.com>
2023-08-01 19:20:53 +0530

    fix: required inputs validation

--- src/views/Index.tsx
+++ src/views/Index.tsx
@@ -3,12 +3,14 @@ import AudioInputs, { defaultAudioData } from "./Audio";
 import { Audiogram } from "../audiogram/Root";
 import { Flex, Grid, GridItem, Text } from "@chakra-ui/react";
 import { IAudioInput } from "../interfaces/AudioInputInterface";
-import { every, isEmpty } from "lodash";
 
 interface AudiogramIndexProps {}
 
 const AudiogramIndex: FC<AudiogramIndexProps> = () => {
   let [audioInputs, setAudioInputs] = useState<IAudioInput>(defaultAudioData);
+
+  const hasRequiredInputs =
+    audioInputs.audioFile && audioInputs.srtFile && audioInputs.coverImage;
 
   return (
     <>
@@ -20,7 +22,7 @@ const AudiogramIndex: FC<AudiogramIndexProps> = () => {
           <AudioInputs handleUpload={(data) => setAudioInputs(data)} />
         </GridItem>
         <GridItem>
-          {!every(audioInputs, isEmpty) ? (
+          {hasRequiredInputs ? (
             <Audiogram audioInputs={audioInputs} />
           ) : (
             <Flex alignItems="center" justifyContent="center">
